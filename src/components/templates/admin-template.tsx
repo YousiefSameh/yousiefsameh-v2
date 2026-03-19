a607499ae06e0921a320ffcf03cb61d907451616
Yousief Sameh <yousief.sameh@outlook.com>
2026-03-19 22:51:31 +0200

    feat(admin-projects-core): setup tanstack query

--- src/components/templates/admin-template.tsx
+++ src/components/templates/admin-template.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { AdminSidebar } from "@/components/molecules/AdminSidebar";
 import { AdminHeader } from "@/components/molecules/AdminHeader";
 import { auth } from "@/lib/auth";
+import QueryProvider from "./query-provider";
 
 export default async function AdminTemplate({
   children,
@@ -24,7 +25,9 @@ export default async function AdminTemplate({
       <AdminSidebar />
       <div className="flex-1 flex flex-col">
         <AdminHeader user={user} />
-        <main className="flex-1 p-6 ml-0 lg:ml-64">{children}</main>
+        <main className="flex-1 p-6 ml-0 lg:ml-64">
+          <QueryProvider>{children}</QueryProvider>
+        </main>
       </div>
     </div>
   );
